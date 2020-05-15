85d2f0896f8d8fbc5335dd062d6d7e940c282701
Theo Gravity <theo@suteki.nu>
2020-05-15 12:34:46 -0700

    CI fixes.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { BaseError } from './error-types/BaseError'
 import { ErrorRegistry } from './ErrorRegistry'
 
-export { BaseError, ErrorRegistry }
+export {
+  BaseError,
+  ErrorRegistry
+}
